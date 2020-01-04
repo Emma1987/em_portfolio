2cc557f714ba87954500aad00f1ed3eba3070d13
Emma1987 <mercadal.em@gmail.com>
2020-01-04 15:06:26 +0100

    Add navbar and footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,6 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.scss');
 require('../css/home_page.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
